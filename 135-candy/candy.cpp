3b833bd61c71adc82d9494436ec6c589d8d6c520
Bhargav Patel <102641313+Bhargav8120@users.noreply.github.com>
2026-09-02 09:37:50 +0530

    Time: 4 ms (27.09%) | Memory: 71.5 MB (9.68%) - LeetSync

--- 135-candy/candy.cpp
+++ 135-candy/candy.cpp
@@ -8,7 +8,7 @@ public:
 
         vector<int> left(n,1);
 
-        vector<int> right(n,1);
+        // vector<int> right(n,1);
 
         for(int i=1;i<n;i++){
             if(ratings[i]>ratings[i-1]){
@@ -16,19 +16,25 @@ public:
             }
         }
 
+        int curr=1;
+        int right=1;
+
+        int sum=max(1,left[n-1]);
+
         for(int i=n-2;i>=0;i--){
             if(ratings[i]>ratings[i+1]){
-                right[i]=right[i+1]+1;
+                curr=right+1;
             }
+            else{
+                curr=1;
+            }
+
+            right=curr;
+
+            sum+=max(left[i],curr);
         }
 
-        int ans=0;
-
-        for(int i=0;i<n;i++){
-            ans+=max(left[i],right[i]);
-        }
-
-        return ans;
+        return sum;
 
     }
 };
