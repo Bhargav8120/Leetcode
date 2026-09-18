ef060103cf59e5e09eac486700c75ed1d6107239
Bhargav Patel <102641313+Bhargav8120@users.noreply.github.com>
2026-09-18 20:06:27 +0530

    Time: 11 ms (25.68%) | Memory: 12.4 MB (62.64%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -13,15 +13,13 @@ public:
 
         int ans=0;
 
-        for(int i=0;i<s.size();){
+        for(int i=0;i<s.size();i++){
 
             if(mp[s[i]] < mp[s[i+1]]){
-                ans+=(mp[s[i+1]]-mp[s[i]]);
-                i=i+2;
+                ans-=mp[s[i]];
             }
             else{
                 ans+=mp[s[i]];
-                i++;
             }
         }
 
