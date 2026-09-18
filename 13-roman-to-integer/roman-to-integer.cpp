af3220c28fdf7bce9ac4a34282d9cc690d7f66bf
Bhargav Patel <102641313+Bhargav8120@users.noreply.github.com>
2026-09-18 20:00:28 +0530

    Time: 14 ms (11.41%) | Memory: 12.3 MB (62.64%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -10,47 +10,19 @@ public:
         mp['C']=100;
         mp['D']=500;
         mp['M']=1000;
-        // mp["IV"]=4;
-        // mp["IX"]=9;
-        // mp["XL"]=49;
-        // mp["XC"]=90;
-        // mp["CD"]=400;
-        // mp["CM"]=900;
-
 
         int ans=0;
 
         for(int i=0;i<s.size();){
-            
-            if(i+1<s.size() && s[i]=='I' && s[i+1]=='V'){
-                ans+=4;
-                i=i+2;
-            }
-            else if(i+1<s.size() &&  s[i]=='I' && s[i+1]=='X'){
-                ans+=9;
-                i=i+2;
-            }
-            else if( i+1<s.size() && s[i]=='X' && s[i+1]=='L'){
-                ans+=40;
-                i=i+2;
-            }
-            else if(i+1<s.size() &&  s[i]=='X' && s[i+1]=='C'){
-                ans+=90;
-                i=i+2;
-            }
-            else if(i+1<s.size() && s[i]=='C' && s[i+1]=='D'){
-                ans+=400;
-                i=i+2;
-            }
-            else if(i+1<s.size() && s[i]=='C' && s[i+1]=='M'){
-                ans+=900;
+
+            if(i+1 < s.size() && mp[s[i]] < mp[s[i+1]]){
+                ans+=(mp[s[i+1]]-mp[s[i]]);
                 i=i+2;
             }
             else{
                 ans+=mp[s[i]];
                 i++;
             }
-            
         }
 
         return ans;
